a9b46c183eb5380cfc096234e0fc5a2e5924e6b1
away7000 <83220445+away7000@users.noreply.github.com>
2026-03-30 06:09:28 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const bot = startTelegram(
   () => "Running"
 );
 
-for (let token of tokens.slice(0, 2)) {
+for (let token of tokens.slice(0, 1)) {
 
   // 🛡️ SAFETY CHECK REAL
   const safety = await checkTradeSafety(token);
