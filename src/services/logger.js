2f8c9c693f4ea3f8345ff8f5083ed31149a11819
away7000 <83220445+away7000@users.noreply.github.com>
2026-03-29 21:16:00 +0700

    Update logger.js

--- src/services/logger.js
+++ src/services/logger.js
@@ -2,14 +2,19 @@ import fs from "fs";
 
 const FILE = "./data/trades.json";
 
-export function logTrade(features, result) {
+export function logTrade(features, result, token) {
   let data = [];
 
   if (fs.existsSync(FILE)) {
     data = JSON.parse(fs.readFileSync(FILE));
   }
 
-  data.push({ features, result });
+  data.push({
+    features,
+    result, // 1 profit, 0 rugpull/loss
+    symbol: token.symbol,
+    timestamp: Date.now()
+  });
 
   fs.writeFileSync(FILE, JSON.stringify(data, null, 2));
 }
